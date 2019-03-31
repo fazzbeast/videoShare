00d71b9720cc10dc919b07ee8652baac4799e922
Andres <azourelli@gmail.com>
2019-03-31 10:52:10 -0500

    videoplayer added and is fluid

--- Client/src/VideoQueue/VideoQueue.js
+++ Client/src/VideoQueue/VideoQueue.js
@@ -2,12 +2,22 @@ import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import ReactPlayer from "react-player";
+import "./VideoQueue.css";
 class VideoQueue extends Component {
   render() {
     return (
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-12 col-sm-8">hi</div>
+      <div className="container-fluid vh-50">
+        <div className="row h-100">
+          <div className="col-12 col-sm-8 video-holder">
+            <div className="player-wrapper ">
+              <ReactPlayer
+                className="react-player"
+                url="https://www.youtube.com/watch?v=rnP74z0n3Ms"
+                width="100%"
+                height="100%"
+              />
+            </div>
+          </div>
           <div className="col-12 col-sm-4">hi</div>
         </div>
       </div>
