80d6d1286e51cac43339b8dd91549d071942b376
Andres <azourelli@gmail.com>
2019-03-31 00:49:41 -0500

    added more

--- Client/src/VideoQueue/VideoQueue.js
+++ Client/src/VideoQueue/VideoQueue.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
-
+import ReactPlayer from "react-player";
 class VideoQueue extends Component {
   render() {
-    return <div>h1</div>;
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12 col-sm-8">hi</div>
+          <div className="col-12 col-sm-4">hi</div>
+        </div>
+      </div>
+    );
   }
 }
 
