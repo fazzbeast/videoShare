f4f27c01de7f4c2d50b69f0586cc9d03bc60ff4e
Andres <azourelli@gmail.com>
2019-04-01 19:47:23 -0500

    fixed ghost dragging

--- Client/src/VideoQueue/VideoQueue.js
+++ Client/src/VideoQueue/VideoQueue.js
@@ -1,75 +1,70 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
-import update from "immutability-helper";
-import {
-  ListGroup,
-  InputGroup,
-  InputGroupText,
-  InputGroupAddon,
-  Input
-} from "reactstrap";
-import ReactPlayer from "react-player";
-import "./VideoQueue.css";
-import VideoQueueList from "./../VideoQueueList/VideoQueueList";
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import update from 'immutability-helper';
+import { ListGroup, InputGroup, InputGroupText, InputGroupAddon, Input } from 'reactstrap';
+import ReactPlayer from 'react-player';
+import './VideoQueue.css';
+import VideoQueueList from './../VideoQueueList/VideoQueueList';
 
 class VideoQueue extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      url: "https://www.youtube.com/watch?v=ajAfZEDZKCI",
-      videos: [
-        { url: "https://www.youtube.com/watch?v=ajAfZEDZKCI", id: 1 },
-        { url: "https://www.youtube.com/watch?v=rUg0bi74X2o", id: 2 }
-      ]
-    };
-  }
-  render() {
-    const moveCard = (dragIndex, hoverIndex) => {
-      const dragCard = this.state.videos[dragIndex];
-      let newState = update(this.state.videos, {
-        $splice: [[dragIndex, 1], [hoverIndex, 0, dragCard]]
-      });
-      this.setState({ videos: newState });
-    };
-    let loop = this.state.videos.map((data, idx) => (
-      <VideoQueueList
-        Data={data}
-        handleDrop={idx => this.deleteItem(idx)}
-        key={data.id}
-        index={idx}
-        id={data.id}
-        moveCard={moveCard}
-      />
-    ));
-    return (
-      <div className="container-fluid vh-50">
-        <div className="row h-100">
-          <div className="col-12 col-sm-8 video-holder">
-            <div className="player-wrapper ">
-              <ReactPlayer
-                className="react-player"
-                url="https://www.youtube.com/watch?v=rnP74z0n3Ms"
-                width="100%"
-                height="100%"
-              />
-            </div>
-          </div>
-          <div className="col-12 col-sm-4">
-            <h1>Up Next</h1>
-            <div className="mb-2">
-              <InputGroup>
-                <InputGroupAddon addonType="prepend">
-                  <InputGroupText>Add to Queue</InputGroupText>
-                </InputGroupAddon>
-                <Input />
-              </InputGroup>
-            </div>
-            <ListGroup className="clearfix">{loop}</ListGroup>
-          </div>
-        </div>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			url: 'https://www.youtube.com/watch?v=ajAfZEDZKCI',
+			videos: [
+				{ url: 'https://www.youtube.com/watch?v=ajAfZEDZKCI', id: 1 },
+				{ url: 'https://www.youtube.com/watch?v=rUg0bi74X2o', id: 2 },
+				{ url: 'https://www.youtube.com/watch?v=W1FmNWPPTeM', id: 3 }
+			]
+		};
+	}
+	render() {
+		const moveCard = (dragIndex, hoverIndex) => {
+			const dragCard = this.state.videos[dragIndex];
+			let newState = update(this.state.videos, {
+				$splice: [ [ dragIndex, 1 ], [ hoverIndex, 0, dragCard ] ]
+			});
+			this.setState({ videos: newState });
+		};
+		let loop = this.state.videos.map((data, idx) => (
+			<VideoQueueList
+				Data={data}
+				handleDrop={(idx) => this.deleteItem(idx)}
+				key={data.id}
+				index={idx}
+				id={data.id}
+				moveCard={moveCard}
+			/>
+		));
+		return (
+			<div className="container-fluid vh-50">
+				<div className="row h-100">
+					<div className="col-12 col-sm-8 video-holder">
+						<div className="player-wrapper ">
+							<ReactPlayer
+								className="react-player-main"
+								url="https://www.youtube.com/watch?v=rnP74z0n3Ms"
+								width="100%"
+								height="100%"
+							/>
+						</div>
+					</div>
+					<div className="col-12 col-sm-4">
+						<h1>Up Next</h1>
+						<div className="mb-2">
+							<InputGroup>
+								<InputGroupAddon addonType="prepend">
+									<InputGroupText>Add to Queue</InputGroupText>
+								</InputGroupAddon>
+								<Input />
+							</InputGroup>
+						</div>
+						<ListGroup className="clearfix">{loop}</ListGroup>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default withRouter(VideoQueue);
--- Client/src/VideoQueueList/VideoQueueList.js
+++ Client/src/VideoQueueList/VideoQueueList.js
@@ -1,104 +1,102 @@
-import React, { useImperativeHandle, useRef } from "react";
-import { DragSource, DropTarget } from "react-dnd";
+import React, { useImperativeHandle, useRef } from 'react';
+import { DragSource, DropTarget } from 'react-dnd';
 
-import { ListGroupItem } from "reactstrap";
-import ReactPlayer from "react-player";
-import "./VideoQueueList.css";
+import { ListGroupItem } from 'reactstrap';
+import ReactPlayer from 'react-player';
+import './VideoQueueList.css';
 
-const VideoQueueList = React.forwardRef(
-  ({ Data, isDragging, connectDragSource, connectDropTarget }, ref) => {
-    const elementRef = useRef(null);
-    connectDragSource(elementRef);
-    connectDropTarget(elementRef);
-    const opacity = isDragging ? 0 : 1;
-    useImperativeHandle(ref, () => ({
-      getNode: () => elementRef.current
-    }));
-    return (
-      <div ref={elementRef} style={{ opacity: opacity }}>
-        <ListGroupItem style={{ height: "150px" }} className="items">
-          <div className="thumbnail float-right">
-            <ReactPlayer
-              light={true}
-              className="react-player"
-              url={Data.url}
-              height="94px"
-              width="168px"
-              config={{
-                youtube: {
-                  playerVars: { modestbranding: 0 }
-                }
-              }}
-            />
-          </div>
-        </ListGroupItem>
-      </div>
-    );
-  }
-);
+const VideoQueueList = React.forwardRef(({ Data, isDragging, connectDragSource, connectDropTarget }, ref) => {
+	const elementRef = useRef(null);
+	connectDragSource(elementRef);
+	connectDropTarget(elementRef);
+	const opacity = isDragging ? 0 : 1;
+	useImperativeHandle(ref, () => ({
+		getNode: () => elementRef.current
+	}));
+	return (
+		<div ref={elementRef} style={{ opacity: opacity }}>
+			<ListGroupItem style={{ height: '150px' }} className="items">
+				<div className="thumbnail float-right">
+					<ReactPlayer
+						light={true}
+						className="react-player-sub"
+						url={Data.url}
+						height="94px"
+						width="168px"
+						config={{
+							youtube: {
+								playerVars: { modestbranding: 0 }
+							}
+						}}
+					/>
+				</div>
+			</ListGroupItem>
+		</div>
+	);
+});
 
 export default DropTarget(
-  "item",
-  {
-    hover(props, monitor, component) {
-      if (!component) {
-        return null;
-      }
-      // node = HTML Div element from imperative API
-      const node = component.getNode();
-      if (!node) {
-        return null;
-      }
-      const dragIndex = monitor.getItem().index;
-      const hoverIndex = props.index;
-      // Don't replace items with themselves
-      if (dragIndex === hoverIndex) {
-        return;
-      }
-      // Determine rectangle on screen
-      const hoverBoundingRect = node.getBoundingClientRect();
-      // Get vertical middle
-      const hoverMiddleY =
-        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-      // Determine mouse position
-      const clientOffset = monitor.getClientOffset();
-      // Get pixels to the top
-      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
-      // Only perform the move when the mouse has crossed half of the items height
-      // When dragging downwards, only move when the cursor is below 50%
-      // When dragging upwards, only move when the cursor is above 50%
-      // Dragging downwards
-      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-        return;
-      }
-      // Dragging upwards
-      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-        return;
-      }
-      // Time to actually perform the action
-      props.moveCard(dragIndex, hoverIndex);
-      // Note: we're mutating the monitor item here!
-      // Generally it's better to avoid mutations,
-      // but it's good here for the sake of performance
-      // to avoid expensive index searches.
-      monitor.getItem().index = hoverIndex;
-    }
-  },
-  connect => ({
-    connectDropTarget: connect.dropTarget()
-  })
+	'item',
+	{
+		hover(props, monitor, component) {
+			if (!component) {
+				return null;
+			}
+			// node = HTML Div element from imperative API
+			const node = component.getNode();
+			if (!node) {
+				return null;
+			}
+			const dragIndex = monitor.getItem().index;
+			const hoverIndex = props.index;
+			// Don't replace items with themselves
+			if (dragIndex === hoverIndex) {
+				return;
+			}
+			// Determine rectangle on screen
+			const hoverBoundingRect = node.getBoundingClientRect();
+
+			// Get vertical middle
+			const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+			// Determine mouse position
+			const clientOffset = monitor.getClientOffset();
+			// Get pixels to the top
+			const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+			// Only perform the move when the mouse has crossed half of the items height
+			// When dragging downwards, only move when the cursor is below 50%
+			// When dragging upwards, only move when the cursor is above 50%
+			// Dragging downwards
+			if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+				return;
+			}
+			// Dragging upwards
+			if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+				return;
+			}
+			// Time to actually perform the action
+			props.moveCard(dragIndex, hoverIndex);
+			// Note: we're mutating the monitor item here!
+			// Generally it's better to avoid mutations,
+			// but it's good here for the sake of performance
+			// to avoid expensive index searches.
+			monitor.getItem().index = hoverIndex;
+		}
+	},
+	(connect) => ({
+		connectDropTarget: connect.dropTarget()
+	})
 )(
-  DragSource(
-    "item",
-    {
-      beginDrag: props => ({
-        id: props.id,
-        index: props.index
-      })
-    },
-    (connect, monitor) => ({
-      connectDragSource: connect.dragSource(),
-      isDragging: monitor.isDragging()
-    })
-  )(VideoQueueList)
+	DragSource(
+		'item',
+		{
+			beginDrag: (props) => ({
+				id: props.id,
+				index: props.index
+			})
+		},
+		(connect, monitor) => ({
+			connectDragSource: connect.dragSource(),
+			isDragging: monitor.isDragging()
+		})
+	)(VideoQueueList)
 );
