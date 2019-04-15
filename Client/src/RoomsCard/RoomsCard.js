5b348105f0d7a7eec8c57d75aa07130e42181f36
Andres <azourelli@gmail.com>
2019-04-15 17:25:31 -0400

    fixed rooms link share button

--- Client/src/RoomsCard/RoomsCard.js
+++ Client/src/RoomsCard/RoomsCard.js
@@ -1,48 +1,83 @@
-import React, { Component } from 'react';
-import { ListGroupItem, Button } from 'reactstrap';
-import { withRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import { ListGroupItem, Button } from "reactstrap";
+import { withRouter } from "react-router-dom";
 class RoomsCard extends Component {
-	onClick = (id) => {
-		this.props.history.push(`/VideoQueue/${id}`);
-	};
+  constructor(props) {
+    super(props);
 
-	add = (id) => {
-		const name = `http://localhost:3000/VideoQueue/${id}`;
-		return <input type="text" value={name} className="float-right" readOnly onClick={(e) => e.stopPropagation()} />;
-	};
+    this.state = {
+      isItemContentVisible: {}
+    };
+  }
 
-	showShare = (e) => {
-		e.stopPropagation();
-		this.props.showShare();
-	};
-	render() {
-		const shared = (id) =>
-			this.props.valueShare ? (
-				this.add(id)
-			) : (
-				<Button onClick={(e) => this.showShare(e)} className="float-right" color="primary" size="sm">
-					Share
-				</Button>
-			);
-		const data = this.props.roomData || [];
-		const display = data.map((item) => (
-			<ListGroupItem key={item.roomID} className="pointer" onClick={() => this.onClick(item.roomID)}>
-				<h4 className="float-left">{item.roomName}</h4>
-				<Button
-					size="sm"
-					className="ml-1 float-right btn-danger"
-					onClick={(e) => this.props.onDeletes(e, item.roomID)}>
-					Delete
-				</Button>
-				{shared(item.roomID)}
-			</ListGroupItem>
-		));
-		const noVids = (
-			<div>
-				<h3>No Rooms</h3>
-			</div>
-		);
-		return <React.Fragment>{this.props.roomData ? display : noVids}</React.Fragment>;
-	}
+  onClick = id => {
+    this.props.history.push(`/VideoQueue/${id}`);
+  };
+
+  add = id => {
+    const name = `http://localhost:3000/VideoQueue/${id}`;
+    return (
+      <input
+        type="text"
+        value={name}
+        className="float-right"
+        readOnly
+        onClick={e => e.stopPropagation()}
+      />
+    );
+  };
+
+  showShare = e => {
+    e.stopPropagation();
+    this.props.showShare();
+  };
+
+  showContent = (e, id) => {
+    e.stopPropagation();
+    this.setState({
+      isItemContentVisible: {
+        ...this.state.isItemContentVisible,
+        [id]: true
+      }
+    });
+  };
+  render() {
+    const shared = id =>
+      this.state.isItemContentVisible[id] ? (
+        this.add(id)
+      ) : (
+        <Button
+          onClick={e => this.showContent(e, id)}
+          className="float-right"
+          color="primary"
+          size="sm">
+          Share
+        </Button>
+      );
+    const data = this.props.roomData || [];
+    const display = data.map(item => (
+      <ListGroupItem
+        key={item.roomID}
+        className="pointer"
+        onClick={() => this.onClick(item.roomID)}>
+        <h4 className="float-left">{item.roomName}</h4>
+        <Button
+          size="sm"
+          className="ml-1 float-right btn-danger"
+          onClick={e => this.props.onDeletes(e, item.roomID)}>
+          Delete
+        </Button>
+        {shared(item.roomID)}
+      </ListGroupItem>
+    ));
+    const noVids = (
+      <div>
+        <h3>No Rooms</h3>
+      </div>
+    );
+    return (
+      <React.Fragment>{this.props.roomData ? display : noVids}</React.Fragment>
+    );
+  }
 }
 export default withRouter(RoomsCard);
