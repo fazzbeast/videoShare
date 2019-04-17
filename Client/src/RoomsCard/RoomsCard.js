042488078263475df342f4182f57060279693381
Andres <azourelli@gmail.com>
2019-04-16 20:04:16 -0500

    added copy to clipboard

--- Client/src/RoomsCard/RoomsCard.js
+++ Client/src/RoomsCard/RoomsCard.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { ListGroupItem } from 'reactstrap';
+import { ListGroupItem, Button } from 'reactstrap';
 import { withRouter } from 'react-router-dom';
 import { FaShareSquare, FaTrashAlt } from 'react-icons/fa';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 class RoomsCard extends Component {
 	constructor(props) {
 		super(props);
@@ -17,7 +18,16 @@ class RoomsCard extends Component {
 
 	add = (id) => {
 		const name = window.location.origin + `/VideoQueue/${id}`;
-		return <input type="text" value={name} className="float-right" readOnly onClick={(e) => e.stopPropagation()} />;
+		return (
+			<React.Fragment>
+				<CopyToClipboard text={name}>
+					<Button className="float-right mx-2" onClick={(e) => e.stopPropagation()}>
+						Copy to Clipboard
+					</Button>
+				</CopyToClipboard>
+				<input type="text" value={name} className="float-right" readOnly onClick={(e) => e.stopPropagation()} />
+			</React.Fragment>
+		);
 	};
 
 	showShare = (e) => {
@@ -39,24 +49,24 @@ class RoomsCard extends Component {
 			this.state.isItemContentVisible[id] ? (
 				this.add(id)
 			) : (
-				<FaShareSquare
-					color="white"
-					onClick={(e) => this.showContent(e, id)}
-					className="float-right"
-					size="1.5em"
-				/>
+				<FaShareSquare color="white" onClick={(e) => this.showContent(e, id)} className="" size="1.5em" />
 			);
 		const data = this.props.roomData || [];
 		const display = data.map((item) => (
-			<ListGroupItem key={item.roomID} className="pointer" onClick={() => this.onClick(item.roomID)}>
-				<h4 className="float-left">{item.roomName}</h4>
-				<FaTrashAlt
-					color="white"
-					size="1.5em"
-					className="ml-1 float-right"
-					onClick={(e) => this.props.onDeletes(e, item.roomID)}
-				/>
-				{shared(item.roomID)}
+			<ListGroupItem
+				key={item.roomID}
+				className="pointer d-flex align-items-center w-100"
+				onClick={() => this.onClick(item.roomID)}>
+				<h4 className="float-left w-25">{item.roomName}</h4>
+				<div className="d-flex align-items-center justify-content-end w-75 float-right">
+					{shared(item.roomID)}
+					<FaTrashAlt
+						color="white"
+						size="1.5em"
+						className="ml-1"
+						onClick={(e) => this.props.onDeletes(e, item.roomID)}
+					/>
+				</div>
 			</ListGroupItem>
 		));
 		const noVids = (
