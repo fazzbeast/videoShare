bc57939641a810c7bd787085ea240f8b320dd9ad
Andres <azourelli@gmail.com>
2019-04-20 22:08:53 -0500

    working on home page

--- Client/src/Home/Home.js
+++ Client/src/Home/Home.js
@@ -8,12 +8,15 @@ class Home extends Component {
 		return (
 			<div>
 				{this.props.isLoggedIn === true ? <Redirect to="/Rooms" /> : null}
-				<Jumbotron fluid className="jumbo">
+				<Jumbotron fluid className="jumbo mb-0">
 					<Container fluid>
-						<h1 className="display-3 px-3 text-white font-weight-bold">Welcome to VidShare</h1>
-						<p className="lead text-white">Watch videos with your friends synced across all devices!</p>
+						<h1 className="display-3 px-3  font-weight-bold">Welcome to VidShare</h1>
+						{/* <p className="lead ">Watch videos with your friends synced across all devices!</p> */}
 					</Container>
 				</Jumbotron>
+				<div className="mainBody">
+					<div className="explain bg-light ">a</div>
+				</div>
 			</div>
 		);
 	}
